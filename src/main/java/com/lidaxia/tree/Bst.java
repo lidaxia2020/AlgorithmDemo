df258cb0c67bcaf63df76f3bc1bc51ab4725de95
lidaxia <843180879@qq.com>
2021-12-22 20:57:29 +0800

    add: 二叉树学习

--- src/main/java/com/lidaxia/tree/Bst.java
+++ src/main/java/com/lidaxia/tree/Bst.java
@@ -1,7 +1,12 @@
 package com.lidaxia.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * 二分搜索树
+ *
  * @author lidaxia
  * @version 1.0
  * @date 2021/12/21 20:08
@@ -53,11 +58,11 @@ public class Bst<E extends Comparable<E>> {
     /**
      * 前序遍历
      */
-    public void preOrder(){
-        prePrder(root);
+    public void preOrder() {
+        preOrder(root);
     }
 
-    private void prePrder(Node node){
+    private void preOrder(Node node) {
         if (node == null)
             return;
 
@@ -66,6 +71,75 @@ public class Bst<E extends Comparable<E>> {
         preOrder(node.right);
     }
 
+    /**
+     * 前序遍历  非递归
+     */
+    public void preOrderNR() {
+        Stack<Node> stack = new Stack();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            System.out.println(cur.e);
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+        }
+    }
+
+    /**
+     * 层序遍历
+     */
+    public void levelOrder() {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Node cur = queue.remove();
+            System.out.println(cur.e);
+            if (cur.left != null) {
+                queue.add(cur.left);
+            }
+            if (cur.right != null) {
+                queue.add(cur.right);
+            }
+        }
+    }
+
+    /**
+     * 中序遍历
+     */
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    private void inOrder(Node node) {
+        if (node == null)
+            return;
+
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+    }
+
+    /**
+     * 后续遍历
+     */
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    private void postOrder(Node node) {
+        if (node == null)
+            return;
+
+        inOrder(node.left);
+        inOrder(node.right);
+        System.out.println(node.e);
+    }
+
+
     public int size() {
         return size;
     }
@@ -73,6 +147,33 @@ public class Bst<E extends Comparable<E>> {
     public boolean isEmpty() {
         return size == 0;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        generateBstString(root, 0, sb);
+
+        return sb.toString();
+    }
+
+    private void generateBstString(Node node, int depth, StringBuilder sb) {
+        if (node == null) {
+            sb.append(generateDepthString(depth) + "null\n");
+            return;
+        }
+
+        sb.append(generateDepthString(depth) + node.e + "\n");
+        generateBstString(node.left, depth + 1, sb);
+        generateBstString(node.right, depth + 1, sb);
+    }
+
+    private String generateDepthString(int depth) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < depth; i++)
+            sb.append("--");
+        return sb.toString();
+    }
+
 
     private class Node {
 
---
+++ src/main/java/com/lidaxia/tree/Main.java
@@ -0,0 +1,31 @@
+package com.lidaxia.tree;
+
+import java.util.Arrays;
+
+/**
+ * @author lidaxia
+ * @version 1.0
+ * @date 2021/12/22 19:31
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Bst<Integer> bst = new Bst<>();
+        int[] nums = {5, 3, 6, 8, 4, 2};
+        for (int num: nums){
+            bst.add(num);
+        }
+
+//        System.out.println(bst);
+
+        bst.preOrder();
+        System.out.println();
+        bst.preOrderNR();
+        System.out.println();
+        bst.inOrder();
+        System.out.println();
+        bst.postOrder();
+        System.out.println();
+        bst.levelOrder();
+    }
+}
