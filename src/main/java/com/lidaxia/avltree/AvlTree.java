03715c7eb94e3c27f417b78cff1ef295d4a36201
lidaxia <843180879@qq.com>
2022-01-05 21:14:36 +0800

    add: alv

---
+++ src/main/java/com/lidaxia/avltree/AvlTree.java
@@ -0,0 +1,309 @@
+package com.lidaxia.avltree;
+
+import com.lidaxia.map.BstMap;
+
+import java.util.ArrayList;
+
+/**
+ * @author lidaxia
+ * @version 1.0
+ * @date 2022/1/5 20:05
+ */
+public class AvlTree<K extends Comparable<K>, V> {
+
+    private Node root;
+    private int size;
+
+    public AvlTree() {
+        this.root = null;
+        size = 0;
+    }
+
+
+    public void add(K key, V value) {
+        root = add(root, key, value);
+    }
+
+    private Node add(Node node, K key, V value) {
+        if (node == null) {
+            size++;
+            return new Node(key, value);
+        }
+
+        if (key.compareTo(node.key) < 0)
+            node.left = add(node.left, key, value);
+        else if (key.compareTo(node.key) > 0)
+            node.right = add(node.right, key, value);
+        else
+            node.value = value;
+
+        // 更新height
+        node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
+
+        // 计算平衡因子
+        int balanceFactor = getBalanceFactor(node);
+        if (Math.abs(balanceFactor) > 1)
+            ;
+
+        // 平衡维护
+        // 右旋转
+        if (balanceFactor > 1 && getBalanceFactor(node.left) >= 0)
+            return rightRotate(node);
+
+        // 左旋转
+        if (balanceFactor < -1 && getBalanceFactor(node.right) <= 0)
+            return leftRotate(node);
+
+        return node;
+    }
+
+    /**
+     * 对于节点y 进行右旋转，返回根节点x
+     * y
+     * / \                               x
+     * x   T4        向右旋转（y）        /   \
+     * / \         ---------->          z      y
+     * z   T3                           / \    /\
+     * / \                              T1  T2 T3 T4
+     * T1  T2
+     *
+     * @param y
+     * @return
+     */
+    private Node rightRotate(Node y) {
+        Node x = y.left;
+        Node T3 = x.right;
+
+        // 向右旋转
+        x.right = y;
+        y.left = T3;
+
+        // 更新height
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+
+        return x;
+    }
+
+    private Node leftRotate(Node y) {
+        Node x = y.right;
+        Node T2 = x.left;
+
+        // 左旋转
+        x.left = y;
+        y.right = T2;
+
+        // 更新height
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+
+        return x;
+    }
+
+    public boolean isBalanced() {
+        return isBalanced(root);
+    }
+
+    private boolean isBalanced(Node root) {
+        if (root == null)
+            return true;
+
+        int balanceFactor = getBalanceFactor(root);
+        if (Math.abs(balanceFactor) > 1) {
+            return false;
+        }
+        return isBalanced(root.left) && isBalanced(root.right);
+
+
+    }
+
+
+    public boolean isBst() {
+        ArrayList<K> keys = new ArrayList<>();
+        inOrder(root, keys);
+        for (int i = 1; i < keys.size(); i++) {
+            if (keys.get(i - 1).compareTo(keys.get(i)) > 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    private void inOrder(Node root, ArrayList<K> keys) {
+        if (root == null)
+            return;
+
+        inOrder(root.left, keys);
+        keys.add(root.key);
+        inOrder(root.right, keys);
+
+    }
+
+
+    public V remove(K key) {
+        Node node = getNode(root, key);
+        if (node == null)
+            return null;
+
+
+        node = remove(node, key);
+        return node.value;
+    }
+
+    private Node remove(Node node, K key) {
+        if (node == null)
+            return null;
+        if (key.compareTo(node.key) < 0) {
+            node.left = remove(node.left, key);
+            return node;
+        } else if (key.compareTo(node.key) > 0) {
+            node.right = remove(node.right, key);
+            return node;
+        } else {
+            if (node.left == null) {
+                Node rightNode = node.right;
+                node.right = null;
+                size--;
+                return rightNode;
+            }
+
+            if (node.right == null) {
+                Node leftNode = node.left;
+                node.left = null;
+                size--;
+                return leftNode;
+            }
+
+            /**
+             * 待删除节点均不为空
+             */
+            Node successor = minimum(node.right);
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+            node.left = node.right = null;
+            return successor;
+        }
+
+    }
+
+    private Node minimum(Node node) {
+        if (node.left == null)
+            return node;
+        return minimum(node.left);
+    }
+
+    /**
+     * 删除最小值
+     *
+     * @return
+     */
+    public V removeMin() {
+        V ret = minimum();
+        root = removeMin(root);
+        return ret;
+    }
+
+    public V minimum() {
+        if (size == 0)
+            throw new IllegalArgumentException("Bst is empty");
+
+        return minimum(root).value;
+    }
+
+    private Node removeMin(Node node) {
+        if (node.left == null) {
+            Node rightNode = node.right;
+            node.right = null;
+            size--;
+            return rightNode;
+        }
+
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+
+    public boolean contains(K key) {
+        return getNode(root, key) != null;
+    }
+
+
+    public V get(K key) {
+        Node node = getNode(root, key);
+        return node == null ? null : node.value;
+    }
+
+
+    public void set(K key, V newValue) {
+        Node node = getNode(root, key);
+        if (node == null)
+            throw new IllegalArgumentException(key + "doesn't exist!");
+        node.value = newValue;
+    }
+
+
+    public int getSize() {
+        return size;
+    }
+
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 得到key所在节点
+     *
+     * @param node
+     * @param key
+     * @return
+     */
+    private Node getNode(Node node, K key) {
+        if (node == null)
+            return null;
+        if (key.compareTo(node.key) == 0)
+            return node;
+        else if (key.compareTo(node.key) < 0)
+            return getNode(node.left, key);
+        else
+            return getNode(node.right, key);
+    }
+
+
+    /**
+     * 获得节点高度值
+     *
+     * @param node
+     * @return
+     */
+    private int getHeight(Node node) {
+        if (node == null)
+            return 0;
+        return node.height;
+    }
+
+    private int getBalanceFactor(Node node) {
+        if (node == null)
+            return 0;
+        return getHeight(node.left) - getHeight(node.right);
+    }
+
+    private class Node {
+        public K key;
+        public V value;
+        public Node left, right;
+
+        /**
+         * 节点高度值
+         */
+        public int height;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            left = null;
+            right = null;
+            height = 1;
+        }
+    }
+}
